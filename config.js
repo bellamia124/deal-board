d9dd3ada4270501814afa08f01c80460d77be5af
bellamia124 <jasoncurcio@comcast.net>
2026-09-17 17:39:10 -0400

    Custom-domain prep: clean root /valuation & /value paths

---
+++ config.js
@@ -0,0 +1,4 @@
+window.DEAL_BOARD_CONFIG = {
+  SUPABASE_URL: "https://bisudvsrxmsuyuhgozcs.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpc3VkdnNyeG1zdXl1aGdvemNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNTYxMDcsImV4cCI6MjEwMzkzMjEwN30.gAOa3ncQ_ahV9NM5bNb96DvfzDydBhbkMl3N-8Wy-08",
+};
